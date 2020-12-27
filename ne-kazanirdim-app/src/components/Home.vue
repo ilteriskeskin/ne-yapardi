ce4dd8c0c709d677b5c2226b3b717153c6ad0ded
Ali İlteriş Keskin <aliilteriskeskin@gmail.com>
2020-12-27 21:02:36 +0300

    Bug fixed

--- ne-kazanirdim-app/src/components/Home.vue
+++ ne-kazanirdim-app/src/components/Home.vue
@@ -1,44 +1,29 @@
 <template>
   <div class="section">
-
     <!--<div class="title">
       <h1>Ne Kadar Kazanırım</h1>
     </div>-->
 
     <div class="main">
       <div class="main-calculate">
-        
         <div class="main-calculate-title">
           <h1>Ne Kadar Kazanırım</h1>
         </div>
 
         <div class="form-group row m-0">
-          <label for="example-date-input" class="col-12 col-form-label calculate-label">
-            Investment Date
+          <label
+            for="example-date-input"
+            class="col-12 col-form-label calculate-label"
+          >
+            Yatırım Tarihi
           </label>
           <div class="col-8">
             <input
-              class     = "form-control calculate-input"
-              type      = "date"
-              value     = "2011-08-19"
-              id        = "example-date-input"
-              v-model   = "investment_date"/>
-          </div>
-        </div>
-
-        <br />
-
-        <div class="form-group row m-0">
-          <label for="example-date-input" class="col-12 col-form-label calculate-label">
-            Date
-          </label>
-          <div class="col-8">
-            <input
-              class    = "form-control calculate-input"
-              type     = "date"
-              value    = "2011-08-19"
-              id       = "example-date-input"
-              v-model  = "date"
+              class="form-control calculate-input"
+              type="date"
+              value="2011-08-19"
+              id="example-date-input"
+              v-model="investment_date"
             />
           </div>
         </div>
@@ -46,11 +31,39 @@
         <br />
 
         <div class="form-group row m-0">
-          <label for="example-date-input" class="col-12 col-form-label calculate-label">
-            Money
+          <label
+            for="example-date-input"
+            class="col-12 col-form-label calculate-label"
+          >
+            Hedef Tarih
           </label>
           <div class="col-8">
-            <input type="number" placeholder="Number" v-model="money" class="col-10 calculate-input" />
+            <input
+              class="form-control calculate-input"
+              type="date"
+              value="2011-08-19"
+              id="example-date-input"
+              v-model="date"
+            />
+          </div>
+        </div>
+
+        <br />
+
+        <div class="form-group row m-0">
+          <label
+            for="example-date-input"
+            class="col-12 col-form-label calculate-label"
+          >
+            Yatırım Miktarı ve Birimi
+          </label>
+          <div class="col-8">
+            <input
+              type="number"
+              placeholder="Number"
+              v-model="money"
+              class="col-10 calculate-input"
+            />
             <select v-model="currency" class="calculate-input col-2">
               <option value="default" disabled selected>Birim</option>
               <option v-for="(curr, index) in currencies" :key="index">
@@ -63,8 +76,11 @@
         <br />
 
         <div class="form-group row m-0">
-          <label for="example-date-input" class="col-12 col-form-label calculate-label">
-            Target Currency
+          <label
+            for="example-date-input"
+            class="col-12 col-form-label calculate-label"
+          >
+            Hedef Yatırım Birim
           </label>
           <div class="col-8">
             <select v-model="second_currency" class="col-12 calculate-input">
@@ -78,33 +94,53 @@
 
         <br />
 
-        <button class="form-group btn btn-primary calculate-button" @click="calc()">
-          Calculate
+        <button
+          class="form-group btn btn-primary calculate-button"
+          @click="calc()"
+        >
+          Hesapla
         </button>
 
         <br />
         <br />
 
-        <div v-if="investment_target_currency_value != 0" class="calculate-result">
+        <div
+          v-if="investment_target_currency_value != 0"
+          class="calculate-result"
+        >
           <div class="calculate-result-block">
-            <span class="calculate-result-block-title">Yatırdığınız Para: </span>
-            <span class="calculate-result-block-content"><strong>{{ this.money }}</strong> {{ currency }}</span>
+            <span class="calculate-result-block-title"
+              >Yatırdığınız Para:
+            </span>
+            <span class="calculate-result-block-content"
+              ><strong>{{ this.money }}</strong> {{ currency }}</span
+            >
           </div>
 
           <div class="calculate-result-block">
-            <span class="calculate-result-block-title">Yatırdığınız Paranın {{ investment_date }} Tarihindeki Karşılığı:</span>
-            <span class="calculate-result-block-content">{{ investment_target_currency_value }} {{ second_currency }}</span>
+            <span class="calculate-result-block-title"
+              >Yatırdığınız Paranın {{ investment_date }} Tarihindeki
+              Karşılığı:</span
+            >
+            <span class="calculate-result-block-content"
+              >{{ investment_target_currency_value }}
+              {{ second_currency }}</span
+            >
           </div>
 
           <div class="calculate-result-block">
-            <span class="calculate-result-block-title">Yatırdığınız Paranın {{ date }} Tarihindeki Karşılığı:</span>
-            <span class="calculate-result-block-content"><strong> {{ now_target_currency_value }} </strong> {{ second_currency }}</span>
-          </div>          
+            <span class="calculate-result-block-title"
+              >Yatırdığınız Paranın {{ date }} Tarihindeki Karşılığı:</span
+            >
+            <span class="calculate-result-block-content"
+              ><strong> {{ now_target_currency_value }} </strong>
+              {{ second_currency }}</span
+            >
+          </div>
         </div>
-
       </div>
       <div class="main-image">
-        <img src="../assets/img/escobar.jpg">
+        <img src="../assets/img/escobar.jpg" />
       </div>
     </div>
   </div>
@@ -149,74 +185,80 @@ export default {
 
   methods: {
     calc() {
-      if (this.currency === "TRY" && this.date.substring(0,4) < 2005) {
-        let investment_url = 
-        "https://api.exchangeratesapi.io/" +
-        this.investment_date +
-        "?base=" +
-        "TRL";
-      }
-      else{
-          let investment_url = 
+      var investment_url = "";
+
+      if (
+        this.currency === "TRY" &&
+        (this.investment_date.substring(0, 4) <= 2005 ||
+          this.date.substring(0, 4) <= 2005)
+      ) {
+        investment_url =
+          "https://api.exchangeratesapi.io/" +
+          this.investment_date +
+          "?base=" +
+          "TRL";
+      } else {
+        investment_url =
           "https://api.exchangeratesapi.io/" +
           this.investment_date +
           "?base=" +
           this.currency;
       }
 
-
       // date format is 2010-01-12 // 2005-01-02 -- TRL --> TRY
       if (this.investment_date === "" || this.date === "") {
-        this.investment_date = "2002-01-01"
-        this.date = "2005-01-01"
-        // alert("Put the dates on !!");
-        // return 0;
+        alert("Put the inputs on !!");
+        return 0;
       }
 
-            axios.get(investment_url).then((response) => {
-              this.investment_result = response.data;
-              this.calcNow();
-            });                  
-      
-  },
+      axios.get(investment_url).then((response) => {
+        this.investment_result = response.data;
+        this.calcNow();
+      });
+    },
 
     calcNow() {
-      let url =
-        "https://api.exchangeratesapi.io/" +
-        this.date +
-        "?base=" +
-        this.currency;
+      var url = "";
 
-        axios.get(url).then((response) => {
-          this.now_result = response.data;
-          this.mainCalc();
-        });      
+      if (
+        this.currency === "TRY" &&
+        (this.investment_date.substring(0, 4) <= 2005 ||
+          this.date.substring(0, 4) <= 2005)
+      ) {
+        url = "https://api.exchangeratesapi.io/" + this.date + "?base=" + "TRL";
+      } else {
+        url =
+          "https://api.exchangeratesapi.io/" +
+          this.date +
+          "?base=" +
+          this.currency;
+      }
+
+      axios.get(url).then((response) => {
+        this.now_result = response.data;
+        this.mainCalc();
+      });
     },
 
     mainCalc() {
-      if (isNaN(this.investment_result.rates[this.second_currency] )) {
-
+      if (isNaN(this.investment_result.rates[this.second_currency])) {
         this.investment_target_currency_value =
           this.investment_result.rates["TRL"] * this.money;
 
         if (isNaN(this.now_result.rates[this.second_currency])) {
           this.now_target_currency_value =
             this.now_result.rates["TRL"] * this.money;
+        } else if (this.now_result.rates[this.second_currency]) {
+          this.now_target_currency_value =
+            this.now_result.rates[this.second_currency] * this.money;
         }
-        else if (this.now_result.rates[this.second_currency]){
-          this.now_target_currency_value = 
-          this.now_result.rates[this.second_currency] * this.money;
-        }
-      }
-
-      else {
+      } else {
         this.investment_target_currency_value =
-        this.investment_result.rates[this.second_currency] * this.money;
+          this.investment_result.rates[this.second_currency] * this.money;
 
         this.now_target_currency_value =
-        this.now_result.rates[this.second_currency] * this.money;
+          this.now_result.rates[this.second_currency] * this.money;
       }
-
     },
   },
 };
@@ -231,7 +273,7 @@ fixShadow          : 0px 0px 1px 1px rgba(0,0,0,0.10);
 */
 
 * {
-  font-family: 'Oxygen', sans-serif;
+  font-family: "Oxygen", sans-serif;
 }
 
 .section {
@@ -244,7 +286,6 @@ fixShadow          : 0px 0px 1px 1px rgba(0,0,0,0.10);
   display: flex;
   align-items: flex-end;
 }
-
 
 .main {
   width: 100%;
@@ -286,14 +327,14 @@ fixShadow          : 0px 0px 1px 1px rgba(0,0,0,0.10);
   height: 100vh;
   width: 100%;
   object-fit: cover;
-  -webkit-box-shadow: 0px 0px 15px 5px rgba(0,0,0,0.4); 
-  box-shadow: 0px 0px 15px 5px rgba(82, 64, 64, 0.4); 
+  -webkit-box-shadow: 0px 0px 15px 5px rgba(0, 0, 0, 0.4);
+  box-shadow: 0px 0px 15px 5px rgba(82, 64, 64, 0.4);
 }
 
 .calculate-input {
   appearance: none;
   padding: 15px 20px;
-  box-shadow: 0px 0px 1px 1px rgba(0,0,0,0.10);
+  box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
   border: none;
   background-color: #f4f7fa;
   border-radius: 4px;
@@ -318,7 +359,7 @@ fixShadow          : 0px 0px 1px 1px rgba(0,0,0,0.10);
   justify-content: space-between;
   align-items: center;
   padding-block: 20px;
-  box-shadow: 0px 0px 1px 1px rgba(0,0,0,0.05);
+  box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.05);
   margin-left: 12px;
   padding-left: 10px;
   margin-bottom: 15px;
@@ -337,14 +378,15 @@ fixShadow          : 0px 0px 1px 1px rgba(0,0,0,0.10);
 }
 
 @media only screen and (max-width: 1150px) {
-.main-image {
-  display: none;
-}
-.main-calculate {
-  width: 100%;
-}
-.calculate-result {
-  width: 100%;
-}
+  .main-image {
+    display: none;
+  }
+  .main-calculate {
+    width: 100%;
+    height: 100%;
+  }
+  .calculate-result {
+    width: 100%;
+  }
 }
 </style>
